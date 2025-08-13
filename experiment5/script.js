855128654cf897bf91736688d727d2d6ab3b5194
exewhyz <araj52444@gmail.com>
2025-08-13 12:42:46 +0530

    05: Dynamic Product Filter

---
+++ experiment5/script.js
@@ -0,0 +1,34 @@
+const products = [
+  { name: "Jeans", category: "clothing" },
+  { name: "Cookbook", category: "books" },
+  { name: "Tshirt", category: "clothing" },
+];
+
+const productsElement = document.querySelector("#products");
+const categoryElement = document.querySelector("#category");
+
+function filter(category) {
+  if (category === "all") {
+    return products;
+  } else {
+    return products.filter((value) => {
+      return value.category === category;
+    });
+  }
+}
+
+categoryElement.addEventListener("change", () => {
+  const filtered = filter(categoryElement.value);
+  const productList = filtered.map((value) => {
+    return `<div style="border: 1px solid gray; padding: 10px; margin:5px; display:inline-block;">${value.name}</div>`;
+  });
+  const data = productList.join("");
+  productsElement.innerHTML = data;
+});
+
+const filtered = filter("all");
+const productList = filtered.map((value) => {
+  return `<div style="border: 1px solid gray; padding: 10px; margin:5px; display:inline-block;">${value.name}</div>`;
+});
+const data = productList.join("");
+productsElement.innerHTML = data;
